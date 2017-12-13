8c63a02812c5f89a44726532135d9d06caac968e
Tiffany Chen <tiffanysta.c@gmail.com>
2017-12-13 14:00:44 -0500

    finished assignment

--- find.js
+++ find.js
@@ -1,13 +1,12 @@
 var box = document.getElementById("box");
 var boxHeight = box.offsetHeight;
 var boxWidth = box.offsetWidth;
-var style = document.getElementsByTagName("style");
+var style = document.getElementsByTagName("style")[0];
 
 //hardcode target as center
 //randomize later
-var targetX = boxWidth / 2;
-var targetY = boxHeight / 2;
-
+var targetX = Math.random() * boxWidth;
+var targetY = Math.random() * boxHeight;
 
 console.log( "box height: " + boxHeight );
 console.log( "box width: " + boxWidth );
@@ -19,25 +18,42 @@ var distance = function (x0, y0, x1, y1) {
     return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
 };
 
+//popup function!
+
+var popup = function(e) {
+    alert("You found the cow!");
+};
+
 var findIt = function(e) {
     dist = distance(targetX, targetY, e.clientX, e.clientY);
     max = distance(targetX, targetY, boxWidth, boxHeight);
+    console.log(dist);
+    console.log(style.innerHTML);
     ratio = dist / max * 100;
-    if (ratio < 20) {
-        style.innerHTML = style.innerHTML + "\n"
-    };
+    if (ratio < 10) {
+        style.innerHTML = style.innerHTML + "body {background-color: white;} div {cursor:pointer;}";
+        document.addEventListener("click", popup);
+    }
     else if (ratio < 40) {
-        
-    };
+        style.innerHTML = style.innerHTML + "body {background-color: #D3D3D3;} div {cursor:default;}";
+        document.removeEventListener("click", popup);
+    }
+    else if (ratio < 40) {
+        style.innerHTML = style.innerHTML + "body {background-color: #A0A0A0;} div {cursor:default;}";
+        document.removeEventListener("click", popup);
+    }
     else if (ratio < 60) {
-        
-    };
+        style.innerHTML = style.innerHTML + "body {background-color: #606060;} div {cursor:default;}";
+        document.removeEventListener("click", popup);
+    }
     else if (ratio < 80) {
-        
-    };
+        style.innerHTML = style.innerHTML + "body {background-color: #303030;} div {cursor:default;}";
+        document.removeEventListener("click", popup);
+    }
     else {
-
-    };
+        style.innerHTML = style.innerHTML + "body {background-color: black;} div {cursor:default;}";
+        document.removeEventListener("click", popup);
+    }
 };
 
 /*
